7e3f79c37b1deb644064209eb354d1d068db02df
kolyan-droid <kolyan999999999@gmail.com>
2026-08-09 17:28:10 +0300

    edit_8 main.py

--- main.py
+++ main.py
@@ -31,12 +31,11 @@ def answer_callback(call):
     elif call.data.startswith("play:"):
         user_id = call.message.chat.id
         season, seria = call.data.split(":")[1:]
-        key_fo_db = season + ":" + seria
         connection = pool.getconn()
         try:
             with connection.cursor() as cursor:
                 sql_request = "SELECT id_video FROM episodes WHERE episode=%s"
-                cursor.execute(sql_request, (key_fo_db,))
+                cursor.execute(sql_request, f"{season}:{seria}")
                 result = cursor.fetchone()
                 target_value = result[0]
                 sql_request = "SELECT video_id FROM last_video WHERE user_id=%s"
